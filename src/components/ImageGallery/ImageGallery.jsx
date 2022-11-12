85e5a4f07d0b24c754c4f62936e7e167670a2177
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-12 15:56:28 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Gallery } from 'components/Styles/Element.styled';
 import PropTypes from 'prop-types';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
